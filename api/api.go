ae0bd3a63d925ad4d7975835c3deefd0d038ef5c
Daniele Tricoli <eriol@mornie.org>
2024-04-26 02:28:15 +0200

    Invoke WriteHeader only on error
    It's automatically called on the first write with 200 if it's not
    invoked.

--- api/api.go
+++ api/api.go
@@ -21,7 +21,10 @@ func Serve() {
 
 func toJSON(w http.ResponseWriter, status int, value any) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(status)
+
+	if status != http.StatusOK {
+		w.WriteHeader(status)
+	}
 
 	if err := json.NewEncoder(w).Encode(value); err != nil {
 		log.Println(err)
